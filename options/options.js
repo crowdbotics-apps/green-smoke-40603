28d5ccc6dea53ee67ef0e619134c191f3016b40e
Mithilesh@crowdbotics.com <Mithilesh@crowdbotics.com>
2023-05-10 15:39:59 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "green-smoke-40603",
-  url: "https://green-smoke-40603.botics.co",
-  api: "https://green-smoke-40603.botics.co/api/v1"
-}
+  "name": "green-smoke-40603",
+  "url": "https://green-smoke-40603.botics.co",
+  "api": "https://green-smoke-40603.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled4/index.js
@@ -0,0 +1,24 @@
+import { ImageBackground } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled4 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><ImageBackground style={styles.ItJhalSE} source={require("./IMG_20230313_183756.jpg")} resizeMode="cover"></ImageBackground></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  ItJhalSE: {
+    width: 277,
+    height: 476
+  }
+});
+export default Untitled4;
